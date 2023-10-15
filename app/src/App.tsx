03f39b60953f1358fe34ed095e9929bead6b3013
Bercik <robertkaliga13@gmail.com>
2023-10-15 20:36:05 +0200

    adding Button component

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,20 +1,8 @@
 import "./App.scss";
-
-function Avatar() {
-  return (
-    <div className="avatar">
-      <div className="avatar__username">hello!</div>
-      <div className="avatar__other">other!</div>
-    </div>
-  );
-}
+import Button from "./components/Button";
 
 function App() {
-  return (
-    <div className="app__foo">
-      <Avatar />
-    </div>
-  );
+  return <div className="app">Cleaner app</div>;
 }
 
 export default App;
---
+++ app/src/components/Button/index.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./style.scss";
+import { StyleSizes } from "../../types/styles";
+
+type Props = {
+  children: string;
+  OnClick: React.MouseEventHandler<HTMLButtonElement>;
+  size: StyleSizes;
+};
+
+function Button({ children, OnClick, size = "m" }: Props) {
+  function checkSize(size: string) {
+    switch (size) {
+      case "s":
+        return "btn --size-s";
+      case "m":
+        return "btn --size-m";
+      case "l":
+        return "btn --size-l";
+      case "xl":
+        return "btn --size-xl";
+    }
+  }
+  return (
+    <button className={checkSize(size)} onClick={OnClick}>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
